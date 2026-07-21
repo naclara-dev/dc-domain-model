5f86843f7c680664a6da0f2980c9914159a1a054
Ana Clara <anaclrdev@gmail.com>
2026-07-21 14:08:55 -0300

    build: add lombok

--- src/main/java/dev/naclara/dcmodel/model/Character.java
+++ src/main/java/dev/naclara/dcmodel/model/Character.java
@@ -1,16 +1,29 @@
 package dev.naclara.dcmodel.model;
 
+import dev.naclara.dcmodel.Team;
+import dev.naclara.dcmodel.enums.Alignment;
 import dev.naclara.dcmodel.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.time.LocalDate;
-
+@Getter
+@Setter
 public abstract class Character {
+    private int id;
     private String name;
     private String realName;
     private String description;
     private Gender gender;
+    private Alignment alignment;
+    private Team team;
     private float height;
     private float weight;
     private String birthPlace;
     private String creator;
+
+    public Character(String name, Alignment alignment) {
+        this.name = name;
+        this.alignment = alignment;
+    }
 }
